35da223d702ae6336eac7c8f61daa7152a2f60e3
Roshni <roshni.dhadke73@gmail.com>
2022-11-10 14:14:27 +0530

    RadioButton

--- app/src/main/java/com/example/views/MainActivity.kt
+++ app/src/main/java/com/example/views/MainActivity.kt
@@ -1,80 +1,47 @@
 package com.example.views
 
 import android.annotation.SuppressLint
+import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import android.widget.Toast.makeText
 import kotlin.math.tanh
 
 class MainActivity : AppCompatActivity() {
-    var fn:EditText?=null
-    var ln:EditText?=null
-    var ea:EditText?=null
-    var pd:EditText?=null
-    var cancelbtn:Button?=null
-    var proceedbtn:Button?=null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fn = findViewById(R.id.fn)
-        ln = findViewById(R.id.ln)
-        ea = findViewById(R.id.ea)
-        pd = findViewById(R.id.pd)
-        cancelbtn = findViewById(R.id.cancelbtn)
-        proceedbtn = findViewById(R.id.proceedbtn)
-
-        proceedbtn?.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View?) {
-                CheckFields()
-
+       var rg=findViewById<RadioGroup>(R.id.rg)
+        var btn1=findViewById<Button>(R.id.btn1)
+        var btn2=findViewById<Button>(R.id.btn2)
+        var tv2=findViewById<TextView>(R.id.tv2)
+    /*    rg.setOnCheckedChangeListener { radioGroup, i ->
+            val rd=findViewById<RadioButton>(i)
+        } */
+        btn1.setOnClickListener {
+            val a=rg.checkedRadioButtonId
+            var b=findViewById<RadioButton>(a)
+             tv2.text=b.text
+           /* if(a==-1){
+                Toast.makeText(this,"selected atleast one option",Toast.LENGTH_SHORT).show()
             }
-
-        })
-
-        cancelbtn!!.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                finish()
-                System.exit(0)
-            }
-        })
-    }
-
-        fun CheckFields(): Boolean {
-            if (fn!!.length()==0) {
-                fn!!.error = "This field is required"
-                return true
-            }
-            if(ln!!.length()==0){
-                ln!!.error="This fied is required"
-                return true
-            }
-            if(ea!!.length()==0){
-                ea!!.error="This fied is required"
-                return true
-            }
-            if(pd!!.length()==0){
-                pd!!.error="This fied is required"
-                return true
-            }
-            return true
+            else{
+             val rd=findViewById<RadioButton>(a)
+             Toast.makeText(this,rd.text,Toast.LENGTH_SHORT).show()
+            }*/
+        }
+        btn2.setOnClickListener {
+            tv2.text=" "
         }
     }
 
-
-
-
-
+}
 
 
 
